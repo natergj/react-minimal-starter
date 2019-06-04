0b029de30da83fcac5a67fc2b5331b95dff4cf1a
Nater <nathan.jorde@target.com>
2019-06-04 07:24:47 -0500

    example for specificiity issue with material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import {
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+  makeStyles
+} from "@material-ui/core";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    margin: "0px"
+  },
+  expanded: {
+    color: "orange",
+    margin: 0,
+  }
+}));
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      Hello World
+    <div style={{ height: 600, width: 300 }}>
+      <h1>Hello World</h1>
+      <ExpansionPanel classes={classes}>
+        <ExpansionPanelSummary>Title 1</ExpansionPanelSummary>
+        <ExpansionPanelDetails>My Details 1</ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel classes={classes}>
+        <ExpansionPanelSummary>Title 2</ExpansionPanelSummary>
+        <ExpansionPanelDetails>My Details 2</ExpansionPanelDetails>
+      </ExpansionPanel>
     </div>
-  )
+  );
 };
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+const theme = createMuiTheme({});
 
-ReactDOM.render(<App />, document.getElementById("app"));
-
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("app")
+);
